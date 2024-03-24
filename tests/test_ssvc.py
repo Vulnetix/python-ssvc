ba94dfc175a9ef058805c01bd7f75546b51e97f8
Chris Langton <chris@langton.cloud>
2024-03-25 00:06:16 +1100

    fix(test): 100% coverage
    Signed-off-by: Christopher Langton <chris@langton.cloud>

--- tests/test_ssvc.py
+++ tests/test_ssvc.py
@@ -20,6 +20,66 @@ def test_decision_active_no_total_high():
     assert result.impact == ssvc.MissionWellbeingImpact.HIGH, "SSVC decision should be HIGH"
     assert result.action == ssvc.DecisionAction.ACT, "SSVC decision should be ACT"
 
+def test_decision_active_no_partial_high():
+    result = ssvc.Decision(
+        ssvc.ExploitationLevel.ACTIVE,
+        ssvc.Automatable.NO,
+        ssvc.TechnicalImpact.PARTIAL,
+        ssvc.MissionWellbeingImpact.HIGH,
+    ).outcome
+    assert result.impact == ssvc.MissionWellbeingImpact.HIGH, "SSVC decision should be HIGH"
+    assert result.action == ssvc.DecisionAction.ATTEND, "SSVC decision should be ATTEND"
+
+def test_decision_active_no_partial_low():
+    result = ssvc.Decision(
+        ssvc.ExploitationLevel.ACTIVE,
+        ssvc.Automatable.NO,
+        ssvc.TechnicalImpact.PARTIAL,
+        ssvc.MissionWellbeingImpact.LOW,
+    ).outcome
+    assert result.impact == ssvc.MissionWellbeingImpact.LOW, "SSVC decision should be LOW"
+    assert result.action == ssvc.DecisionAction.TRACK, "SSVC decision should be TRACK"
+
+def test_decision_active_no_partial_medium():
+    result = ssvc.Decision(
+        ssvc.ExploitationLevel.ACTIVE,
+        ssvc.Automatable.NO,
+        ssvc.TechnicalImpact.PARTIAL,
+        ssvc.MissionWellbeingImpact.MEDIUM,
+    ).outcome
+    assert result.impact == ssvc.MissionWellbeingImpact.MEDIUM, "SSVC decision should be MEDIUM"
+    assert result.action == ssvc.DecisionAction.ATTEND, "SSVC decision should be ATTEND"
+
+def test_decision_active_no_total_medium():
+    result = ssvc.Decision(
+        ssvc.ExploitationLevel.ACTIVE,
+        ssvc.Automatable.NO,
+        ssvc.TechnicalImpact.TOTAL,
+        ssvc.MissionWellbeingImpact.MEDIUM,
+    ).outcome
+    assert result.impact == ssvc.MissionWellbeingImpact.MEDIUM, "SSVC decision should be MEDIUM"
+    assert result.action == ssvc.DecisionAction.ACT, "SSVC decision should be ACT"
+
+def test_decision_active_no_total_low():
+    result = ssvc.Decision(
+        ssvc.ExploitationLevel.ACTIVE,
+        ssvc.Automatable.NO,
+        ssvc.TechnicalImpact.TOTAL,
+        ssvc.MissionWellbeingImpact.LOW,
+    ).outcome
+    assert result.impact == ssvc.MissionWellbeingImpact.LOW, "SSVC decision should be LOW"
+    assert result.action == ssvc.DecisionAction.ATTEND, "SSVC decision should be ATTEND"
+
+def test_decision_active_yes_total_high():
+    result = ssvc.Decision(
+        ssvc.ExploitationLevel.ACTIVE,
+        ssvc.Automatable.YES,
+        ssvc.TechnicalImpact.TOTAL,
+        ssvc.MissionWellbeingImpact.HIGH,
+    ).outcome
+    assert result.impact == ssvc.MissionWellbeingImpact.LOW, "SSVC decision should be LOW"
+    assert result.action == ssvc.DecisionAction.TRACK, "SSVC decision should be TRACK"
+
 def test_decision_none_no_total_high():
     result = ssvc.Decision(
         ssvc.ExploitationLevel.NONE,
@@ -30,6 +90,26 @@ def test_decision_none_no_total_high():
     assert result.impact == ssvc.MissionWellbeingImpact.HIGH, "SSVC decision should be HIGH"
     assert result.action == ssvc.DecisionAction.TRACK_STAR, "SSVC decision should be TRACK_STAR"
 
+def test_decision_none_yes_total_high():
+    result = ssvc.Decision(
+        ssvc.ExploitationLevel.NONE,
+        ssvc.Automatable.YES,
+        ssvc.TechnicalImpact.TOTAL,
+        ssvc.MissionWellbeingImpact.HIGH,
+    ).outcome
+    assert result.impact == ssvc.MissionWellbeingImpact.LOW, "SSVC decision should be LOW"
+    assert result.action == ssvc.DecisionAction.TRACK, "SSVC decision should be TRACK"
+
+def test_decision_none_no_partial_high():
+    result = ssvc.Decision(
+        ssvc.ExploitationLevel.NONE,
+        ssvc.Automatable.NO,
+        ssvc.TechnicalImpact.PARTIAL,
+        ssvc.MissionWellbeingImpact.HIGH,
+    ).outcome
+    assert result.impact == ssvc.MissionWellbeingImpact.HIGH, "SSVC decision should be HIGH"
+    assert result.action == ssvc.DecisionAction.TRACK, "SSVC decision should be TRACK"
+
 def test_decision_poc_no_total_high():
     result = ssvc.Decision(
         ssvc.ExploitationLevel.POC,
@@ -38,6 +118,26 @@ def test_decision_poc_no_total_high():
         ssvc.MissionWellbeingImpact.HIGH,
     ).outcome
     assert result.impact == ssvc.MissionWellbeingImpact.HIGH, "SSVC decision should be HIGH"
+    assert result.action == ssvc.DecisionAction.ATTEND, "SSVC decision should be ATTEND"
+
+def test_decision_poc_no_total_medium():
+    result = ssvc.Decision(
+        ssvc.ExploitationLevel.POC,
+        ssvc.Automatable.NO,
+        ssvc.TechnicalImpact.TOTAL,
+        ssvc.MissionWellbeingImpact.MEDIUM,
+    ).outcome
+    assert result.impact == ssvc.MissionWellbeingImpact.MEDIUM, "SSVC decision should be MEDIUM"
+    assert result.action == ssvc.DecisionAction.ATTEND, "SSVC decision should be ATTEND"
+
+def test_decision_poc_no_total_low():
+    result = ssvc.Decision(
+        ssvc.ExploitationLevel.POC,
+        ssvc.Automatable.NO,
+        ssvc.TechnicalImpact.TOTAL,
+        ssvc.MissionWellbeingImpact.LOW,
+    ).outcome
+    assert result.impact == ssvc.MissionWellbeingImpact.LOW, "SSVC decision should be LOW"
     assert result.action == ssvc.DecisionAction.ATTEND, "SSVC decision should be ATTEND"
 
 def test_decision_poc_no_partial_high():
