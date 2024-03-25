321022e9e87cb529e78ab608374649eee0d8b60f
Chris Langton <chris@vulnetix.com>
2024-03-25 14:24:28 +1100

    feat: add DecisionPriority outcome

--- src/ssvc/__init__.py
+++ src/ssvc/__init__.py
@@ -18,27 +18,44 @@ class MissionWellbeingImpact(Enum):
     MEDIUM = 'medium'
     HIGH = 'high'
 
+class DecisionPriority(Enum):
+    LOW = 'low'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+    IMMEDIATE = 'immediate'
+
 class DecisionAction(Enum):
     TRACK = 'Track'
     TRACK_STAR = 'Track*'
     ATTEND = 'Attend'
     ACT = 'Act'
 
+priority_map = {
+    DecisionAction.TRACK: DecisionPriority.LOW,
+    DecisionAction.TRACK_STAR: DecisionPriority.MEDIUM,
+    DecisionAction.ATTEND: DecisionPriority.MEDIUM,
+    DecisionAction.ACT: DecisionPriority.IMMEDIATE,
+}
+
 class DecisionOutcome:
     def __init__(self,
-            impact: TechnicalImpact|MissionWellbeingImpact,
             action: DecisionAction
         ):
-        self.impact: TechnicalImpact|MissionWellbeingImpact = impact
+        self.priority: DecisionPriority = priority_map[action]
         self.action: DecisionAction = action
 
 class Decision:
+    exploitation: ExploitationLevel
+    automatable: Automatable
+    technical_impact: TechnicalImpact
+    mission_wellbeing: MissionWellbeingImpact
     outcome: DecisionOutcome
+
     def __init__(self,
-        exploitation: ExploitationLevel,
-        automatable: Automatable,
-        technical_impact: TechnicalImpact,
-        mission_wellbeing: MissionWellbeingImpact
+        exploitation: ExploitationLevel = None,
+        automatable: Automatable = None,
+        technical_impact: TechnicalImpact = None,
+        mission_wellbeing: MissionWellbeingImpact = None
     ):
         if isinstance(exploitation, str):
             exploitation = ExploitationLevel(exploitation)
@@ -49,40 +66,87 @@ class Decision:
         if isinstance(mission_wellbeing, str):
             mission_wellbeing = MissionWellbeingImpact(mission_wellbeing)
 
-        if exploitation == ExploitationLevel.NONE:
-            if automatable == Automatable.YES:
-                self.outcome = DecisionOutcome(MissionWellbeingImpact.LOW, DecisionAction.TRACK)
+        self.exploitation = exploitation
+        self.automatable = automatable
+        self.technical_impact = technical_impact
+        self.mission_wellbeing = mission_wellbeing
+        if all([
+            isinstance(self.exploitation, ExploitationLevel),
+            isinstance(self.automatable, Automatable),
+            isinstance(self.technical_impact, TechnicalImpact),
+            isinstance(self.mission_wellbeing, MissionWellbeingImpact)
+        ]):
+            self.evaluate()
+
+    def evaluate(self) -> DecisionOutcome:
+        if not isinstance(self.exploitation, ExploitationLevel):
+            raise AttributeError('ExploitationLevel has not been provided')
+        if not isinstance(self.automatable, Automatable):
+            raise AttributeError('Automatable has not been provided')
+        if not isinstance(self.technical_impact, TechnicalImpact):
+            raise AttributeError('TechnicalImpact has not been provided')
+        if not isinstance(self.mission_wellbeing, MissionWellbeingImpact):
+            raise AttributeError('MissionWellbeingImpact has not been provided')
+
+        if self.exploitation == ExploitationLevel.NONE:
+            if self.automatable == Automatable.YES:
+                if self.mission_wellbeing == MissionWellbeingImpact.HIGH: # TechnicalImpact.TOTAL or TechnicalImpact.PARTIAL
+                    self.outcome = DecisionOutcome(DecisionAction.ATTEND)
+                else: # MissionWellbeingImpact.LOW MissionWellbeingImpact.MEDIUM and TechnicalImpact.TOTAL or TechnicalImpact.PARTIAL
+                    self.outcome = DecisionOutcome(DecisionAction.TRACK)
             else: # Automatable.NO
-                if technical_impact == TechnicalImpact.PARTIAL:
-                    self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.TRACK)
-                else: # TechnicalImpact.TOTAL
-                    self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.TRACK_STAR)
-        elif exploitation == ExploitationLevel.POC:
-            if automatable == Automatable.YES:
-                self.outcome = DecisionOutcome(MissionWellbeingImpact.LOW, DecisionAction.TRACK)
+                if self.technical_impact == TechnicalImpact.TOTAL and self.mission_wellbeing == MissionWellbeingImpact.HIGH:
+                    self.outcome = DecisionOutcome(DecisionAction.TRACK_STAR)
+                else:  # TechnicalImpact.PARTIAL (all) or TechnicalImpact.TOTAL (low & medium)
+                    self.outcome = DecisionOutcome(DecisionAction.TRACK)
+
+        elif self.exploitation == ExploitationLevel.POC:
+            if self.mission_wellbeing == MissionWellbeingImpact.LOW: # Automatable.NO or Automatable.YES and TechnicalImpact.TOTAL or TechnicalImpact.PARTIAL
+                self.outcome = DecisionOutcome(DecisionAction.TRACK)
+
+            elif self.automatable == Automatable.YES:
+                if self.mission_wellbeing == MissionWellbeingImpact.HIGH: # TechnicalImpact.TOTAL or TechnicalImpact.PARTIAL
+                    self.outcome = DecisionOutcome(DecisionAction.ATTEND)
+                elif self.mission_wellbeing == MissionWellbeingImpact.MEDIUM:
+                    if self.technical_impact == TechnicalImpact.PARTIAL:
+                        self.outcome = DecisionOutcome(DecisionAction.TRACK)
+                    else: # TechnicalImpact.TOTAL
+                        self.outcome = DecisionOutcome(DecisionAction.TRACK_STAR)
             else: # Automatable.NO
-                if technical_impact == TechnicalImpact.PARTIAL:
-                    self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.TRACK)
+                if self.technical_impact == TechnicalImpact.PARTIAL:
+                    if self.mission_wellbeing == MissionWellbeingImpact.HIGH:
+                        self.outcome = DecisionOutcome(DecisionAction.TRACK_STAR)
+                    else: # MissionWellbeingImpact.MEDIUM
+                        self.outcome = DecisionOutcome(DecisionAction.TRACK)
                 else: # TechnicalImpact.TOTAL
-                    if mission_wellbeing == MissionWellbeingImpact.LOW:
-                        self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.ATTEND)
-                    else: # MissionWellbeingImpact.MEDIUM or HIGH
-                        self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.ATTEND)
+                    if self.mission_wellbeing == MissionWellbeingImpact.MEDIUM:
+                        self.outcome = DecisionOutcome(DecisionAction.TRACK_STAR)
+                    else: # MissionWellbeingImpact.HIGH:
+                        self.outcome = DecisionOutcome(DecisionAction.ATTEND)
+
         else: # ExploitationLevel.ACTIVE
-            if automatable == Automatable.YES:
-                self.outcome = DecisionOutcome(MissionWellbeingImpact.LOW, DecisionAction.TRACK)
+            if self.automatable == Automatable.YES:
+                if self.mission_wellbeing == MissionWellbeingImpact.LOW: # TechnicalImpact.TOTAL or TechnicalImpact.PARTIAL
+                    self.outcome = DecisionOutcome(DecisionAction.ATTEND)
+                elif self.mission_wellbeing == MissionWellbeingImpact.HIGH: # TechnicalImpact.TOTAL or TechnicalImpact.PARTIAL
+                    self.outcome = DecisionOutcome(DecisionAction.ACT)
+                else: # MissionWellbeingImpact.MEDIUM
+                    if self.technical_impact == TechnicalImpact.PARTIAL:
+                        self.outcome = DecisionOutcome(DecisionAction.ATTEND)
+                    else: # TechnicalImpact.TOTAL
+                        self.outcome = DecisionOutcome(DecisionAction.ACT)
+
             else: # Automatable.NO
-                if technical_impact == TechnicalImpact.PARTIAL:
-                    if mission_wellbeing == MissionWellbeingImpact.LOW:
-                        self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.TRACK)
-                    elif mission_wellbeing == MissionWellbeingImpact.MEDIUM:
-                        self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.ATTEND)
+                if self.mission_wellbeing == MissionWellbeingImpact.LOW: # TechnicalImpact.TOTAL or TechnicalImpact.PARTIAL
+                    self.outcome = DecisionOutcome(DecisionAction.TRACK)
+                elif self.technical_impact == TechnicalImpact.PARTIAL:
+                    if self.mission_wellbeing == MissionWellbeingImpact.MEDIUM:
+                        self.outcome = DecisionOutcome(DecisionAction.TRACK)
                     else: # MissionWellbeingImpact.HIGH
-                        self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.ATTEND)
+                        self.outcome = DecisionOutcome(DecisionAction.ATTEND)
                 else: # TechnicalImpact.TOTAL
-                    if mission_wellbeing == MissionWellbeingImpact.LOW:
-                        self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.ATTEND)
-                    elif mission_wellbeing == MissionWellbeingImpact.MEDIUM:
-                        self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.ACT)
+                    if self.mission_wellbeing == MissionWellbeingImpact.MEDIUM:
+                        self.outcome = DecisionOutcome(DecisionAction.ATTEND)
                     else: # MissionWellbeingImpact.HIGH
-                        self.outcome = DecisionOutcome(mission_wellbeing, DecisionAction.ACT)
+                        self.outcome = DecisionOutcome(DecisionAction.ACT)
+        return self.outcome
